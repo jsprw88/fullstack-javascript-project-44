05fadfc00fc26b899144cfb3d9100bbbd5e9dc64
jsprw88 <jsprw8@gmail.com>
2025-05-05 21:02:27 +0100

    touch bin/brain-progression.js  src/games/progression.js

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import runProgress from '../src/games/progression.js';
+
+runProgress();
---
+++ src/games/progression.js
@@ -0,0 +1,27 @@
+import runGame from '../index.js';
+import { getRandomNumber } from '../utils.js';
+
+const description = 'What number is missing in the progression?';
+
+const getRoundData = () => {
+    const length = 10;
+    const start = getRandomNumber(1, 20);
+    const step = getRandomNumber(2, 5);
+      
+    const progression = [];
+      
+        for (let i = 0; i < length; i++) {
+          progression.push(start + i * step);
+        }
+
+        const randomIndex = getRandomNumber(0, progression.length - 1);
+        const hiddenValue = progression[randomIndex];
+        
+        progression[randomIndex] = '..';
+    
+    const question = progression;
+    return [question, hiddenValue];
+    };
+    
+    export default () => runGame(description, getRoundData);
+    
