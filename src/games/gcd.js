ae12dcb5e810dc33a57e2fdf5c6003be910441b4
jsprw88 <jsprw8@gmail.com>
2025-05-05 19:33:41 +0100

    touch file bin/brain-gcd.js src/games/gcd.js

---
+++ src/games/gcd.js
@@ -0,0 +1,24 @@
+import runGame from '../index.js';
+import { getRandomNumber } from '../utils.js';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const getRoundData = () => {
+    const a = getRandomNumber();
+    const b = getRandomNumber();
+
+    let num1 = a;
+    let num2 = b;
+
+    while (num2 !== 0) {
+        const temp = num2;
+        num2 = num1 % num2;
+        num1 = temp;
+      }
+    
+    const question = `${a} ${b}`;
+    return [question, num1];
+    };
+    
+    export default () => runGame(description, getRoundData);
+    
