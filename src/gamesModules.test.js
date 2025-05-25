a19dc520fe6d6efa5765b52cd69c600aa042c8e7
jsprw88 <jsprw8@gmail.com>
2025-05-25 16:53:00 +0100

    update gamesModules.test.js

--- src/gamesModules.test.js
+++ src/gamesModules.test.js
@@ -1,22 +1,10 @@
+import { strict as assert } from 'node:assert'
 import { calculate } from './gamesModules.js'
 
-if (calculate(2, '+', 3) !== 5) {
-  throw new Error('Addition operation failed')
-}
+assert(calculate(2, '+', 3) === 5)
+assert(calculate(5, '-', 2) === 3)
+assert(calculate(4, '*', 3) === 12)
+assert(calculate(10, '/', 2) === 5)
+assert(Number.isNaN(calculate(10, '/', 0)))
 
-if (calculate(5, '-', 2) !== 3) {
-  throw new Error('Subtraction operation failed')
-}
-
-if (calculate(4, '*', 3) !== 12) {
-  throw new Error('Multiplication operation failed')
-}
-
-if (calculate(10, '/', 2) !== 5) {
-  throw new Error('Division operation failed')
-}
-
-if (!Number.isNaN(calculate(10, '/', 0))) {
-  throw new Error('Division by zero did not return NaN')
-}
 console.log('All tests passed!')
